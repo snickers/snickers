c4d8d8ccca347bc607e63343929626cda7d56777
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-06-29 17:58:05 -0400

    lib/helpers: remove useless checks

--- lib/helpers.go
+++ lib/helpers.go
@@ -1,29 +1,19 @@
 package lib
 
 import (
-	"fmt"
-
 	"github.com/flavioribeiro/snickers/db"
 )
 
 func ChangeJobStatus(jobID string, newStatus string) {
-	dbInstance, err := db.GetDatabase()
-	if err != nil {
-		fmt.Println("ERROR", err.Error())
-		//		panic(err)
-	}
-	job, err := dbInstance.RetrieveJob(jobID)
+	dbInstance, _ := db.GetDatabase()
+	job, _ := dbInstance.RetrieveJob(jobID)
 	job.Status = newStatus
 	dbInstance.UpdateJob(job.ID, job)
 }
 
 func ChangeJobDetails(jobID string, newDetails string) {
-	dbInstance, err := db.GetDatabase()
-	if err != nil {
-		fmt.Println("ERROR", err.Error())
-		//panic(err)
-	}
-	job, err := dbInstance.RetrieveJob(jobID)
+	dbInstance, _ := db.GetDatabase()
+	job, _ := dbInstance.RetrieveJob(jobID)
 	job.Details = newDetails
 	dbInstance.UpdateJob(job.ID, job)
 }
