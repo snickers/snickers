8051ee93ebb7da6fc2e8ba472005963cff496809
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-07-12 11:20:42 -0600

    s3_uploader: move helper functions to a new file to be used on s3 downloader

--- lib/s3_uploader.go
+++ lib/s3_uploader.go
@@ -1,9 +1,7 @@
 package lib
 
 import (
-	"net/url"
 	"os"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -49,39 +47,5 @@ func S3Upload(jobID string) error {
 	if err != nil {
 		return err
 	}
-	return nil
-}
-
-// GetAWSKey grabs the path and filename for destination
-func GetAWSKey(jobSource string) (string, error) {
-	parsedURL, err := url.Parse(jobSource)
-	if err != nil {
-		return "", err
-	}
-
-	return parsedURL.Path, nil
-}
-
-// GetAWSBucket grabs the bucket from a given s3 source
-func GetAWSBucket(jobSource string) (string, error) {
-	parsedURL, err := url.Parse(jobSource)
-	if err != nil {
-		return "", err
-	}
-
-	region := strings.Split(parsedURL.Host, ".")[0]
-	return region, nil
-}
-
-// SetAWSCredentials will parse the job source and set the credentials
-// on environment variables
-func SetAWSCredentials(jobSource string) error {
-	parsedURL, err := url.Parse(jobSource)
-	if err != nil {
-		return err
-	}
-	os.Setenv("AWS_ACCESS_KEY_ID", parsedURL.User.Username())
-	password, _ := parsedURL.User.Password()
-	os.Setenv("AWS_SECRET_ACCESS_KEY", password)
 	return nil
 }
