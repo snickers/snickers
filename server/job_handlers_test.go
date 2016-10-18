9a85d8731fc8b5c76c463161b4f7c754948d43f8
Maxwell Da Silva <dayvson@gmail.com>
2016-10-18 22:44:26 +0100

    Adding tests for get details and list all jobs

--- server/job_handlers_test.go
+++ server/job_handlers_test.go
@@ -17,9 +17,12 @@ import (
 
 var _ = Describe("Job handler", func() {
 	var (
-		dbInstance db.Storage
-		cfg        gonfig.Gonfig
-		sn         *SnickersServer
+		dbInstance       db.Storage
+		cfg              gonfig.Gonfig
+		sn               *SnickersServer
+		input            types.JobInput
+		jobRecorder      *httptest.ResponseRecorder
+		respJobInputBody map[string]interface{}
 	)
 
 	BeforeEach(func() {
@@ -28,38 +31,74 @@ var _ = Describe("Job handler", func() {
 		dbInstance, _ = db.GetDatabase(cfg)
 		dbInstance.ClearDatabase()
 		sn = New(lagertest.NewTestLogger("job-handler"), cfg, "tcp", ":8000", dbInstance)
+
+		preset := types.Preset{
+			Name: "mp4_1080p",
+		}
+		presetRecorder := httptest.NewRecorder()
+		presetData, _ := json.Marshal(preset)
+		reqPreset, _ := http.NewRequest(http.MethodPost, "/presets", bytes.NewReader(presetData))
+		sn.Handler().ServeHTTP(presetRecorder, reqPreset)
+
+		input = types.JobInput{
+			Source:      "http://s3.example.com/videos/video1.mov",
+			Destination: "s3://example-bucket/future/video1.mp4",
+			PresetName:  "mp4_1080p",
+		}
+
+		jobRecorder = httptest.NewRecorder()
+		payloadData, _ := json.Marshal(input)
+		req, _ := http.NewRequest(http.MethodPost, "/jobs", bytes.NewReader(payloadData))
+
+		sn.Handler().ServeHTTP(jobRecorder, req)
+		json.Unmarshal(jobRecorder.Body.Bytes(), &respJobInputBody)
 	})
 
 	Context("Create job", func() {
 		It("should create a job in the db instance", func() {
-			input := types.JobInput{
-				Source:      "http://s3.example.com/videos/video1.mov",
-				Destination: "s3://example-bucket/future/video1.mp4",
-				PresetName:  "mp4_1080p",
-			}
-			preset := types.Preset{
-				Name: "mp4_1080p",
-			}
-			presetRecorder := httptest.NewRecorder()
-			presetData, _ := json.Marshal(preset)
-			reqPreset, _ := http.NewRequest(http.MethodPost, "/presets", bytes.NewReader(presetData))
-			sn.Handler().ServeHTTP(presetRecorder, reqPreset)
-
-			recorder := httptest.NewRecorder()
-			payloadData, _ := json.Marshal(input)
-			req, _ := http.NewRequest(http.MethodPost, "/jobs", bytes.NewReader(payloadData))
-
-			sn.Handler().ServeHTTP(recorder, req)
-			Expect(recorder.Code).To(BeIdenticalTo(http.StatusCreated))
-			var respBody map[string]interface{}
-			err := json.Unmarshal(recorder.Body.Bytes(), &respBody)
-			Expect(err).NotTo(HaveOccurred())
-			jobID, ok := respBody["id"].(string)
-
+			Expect(jobRecorder.Code).To(BeIdenticalTo(http.StatusCreated))
+			jobID, ok := respJobInputBody["id"].(string)
 			Expect(ok).To(BeIdenticalTo(true))
 			job, err := dbInstance.RetrieveJob(jobID)
 			Expect(err).NotTo(HaveOccurred())
 			Expect(job.Destination).To(BeIdenticalTo(input.Destination))
+		})
+
+		It("should get a given job details", func() {
+			var jobBody map[string]interface{}
+
+			recorder := httptest.NewRecorder()
+			reqJobDetail, _ := http.NewRequest(http.MethodGet, "/jobs/"+respJobInputBody["id"].(string), nil)
+			sn.Handler().ServeHTTP(recorder, reqJobDetail)
+			json.Unmarshal(recorder.Body.Bytes(), &jobBody)
+			Expect(recorder.Code).To(BeIdenticalTo(http.StatusOK))
+			Expect(jobBody["id"]).To(BeIdenticalTo(respJobInputBody["id"]))
+			Expect(jobBody["status"]).To(BeIdenticalTo(respJobInputBody["status"]))
+		})
+
+		It("should list all jobs", func() {
+			secondInput := types.JobInput{
+				Source:      "http://s3.example.com/videos/video2.mov",
+				Destination: "s3://example-bucket/future/video2.mp4",
+				PresetName:  "mp4_1080p",
+			}
+
+			recorder := httptest.NewRecorder()
+			data, _ := json.Marshal(secondInput)
+			req, _ := http.NewRequest(http.MethodPost, "/jobs", bytes.NewReader(data))
+			sn.Handler().ServeHTTP(recorder, req)
+
+			listRecorder := httptest.NewRecorder()
+			var jobListBody []map[string]interface{}
+
+			listJobsRequest, _ := http.NewRequest(http.MethodGet, "/jobs", nil)
+			sn.Handler().ServeHTTP(listRecorder, listJobsRequest)
+			json.Unmarshal(listRecorder.Body.Bytes(), &jobListBody)
+			Expect(listRecorder.Code).To(BeIdenticalTo(http.StatusOK))
+			Expect(len(jobListBody)).To(Equal(2))
+			Expect(jobListBody[0]["source"]).To(BeIdenticalTo("http://s3.example.com/videos/video1.mov"))
+			Expect(jobListBody[1]["source"]).To(BeIdenticalTo(secondInput.Source))
+
 		})
 	})
 })
