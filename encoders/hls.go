44cab7153364aeadcba9a5e8e707204b486428da
Flávio Ribeiro <flavio.ribeiro@nytimes.com>
2016-11-29 23:25:45 -0500

    encoders/hls: transform source in h264 before segmenting, missing tests (refs #147)

--- encoders/hls.go
+++ encoders/hls.go
@@ -1,6 +1,10 @@
 package encoders
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"code.cloudfoundry.org/lager"
 	"github.com/snickers/hls/segmenter"
 	"github.com/snickers/snickers/db"
@@ -22,12 +26,50 @@ func HLSEncode(logger lager.Logger, dbInstance db.Storage, jobID string) error {
 	job.Status = types.JobEncoding
 	dbInstance.UpdateJob(job.ID, job)
 
+	err = encodeInH264(logger, dbInstance, jobID)
+	if err != nil {
+		return err
+	}
 	hlsConfig := buildHLSConfig(job)
 	err = segmenter.Segment(hlsConfig)
 	if err != nil {
 		return err
 	}
 	job.Details = "100%"
+	return nil
+}
+
+func encodeInH264(logger lager.Logger, dbInstance db.Storage, jobID string) error {
+	mp4File, err := ioutil.TempFile("", "snckrs_")
+	mp4File.Close()
+	mp4Filename := mp4File.Name() + ".mp4"
+	os.Create(mp4Filename)
+	defer os.Remove(mp4Filename)
+	if err != nil {
+		return err
+	}
+
+	job, err := dbInstance.RetrieveJob(jobID)
+	if err != nil {
+		return err
+	}
+
+	oldLocalDestination := job.LocalDestination
+	job.LocalDestination = mp4Filename
+	_, err = dbInstance.UpdateJob(jobID, job)
+	if err != nil {
+		return err
+	}
+	err = FFMPEGEncode(logger, dbInstance, jobID)
+	if err != nil {
+		return err
+	}
+	job.LocalDestination = oldLocalDestination
+	job.LocalSource = mp4Filename
+	_, err = dbInstance.UpdateJob(jobID, job)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
